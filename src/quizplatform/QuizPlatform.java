5cebef4b8cb50fb0f1567ca7fdbd1e6cb33094cf
koul1o <kyriakoskyriakopoulos@gmail.com>
2017-03-21 16:17:22 +0100

    Merge origin/master
    Conflicts:
    	src/quizplatform/html/js/quizplatform.js

--- src/quizplatform/QuizPlatform.java
+++ src/quizplatform/QuizPlatform.java
@@ -9,8 +9,10 @@ package quizplatform;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
@@ -31,11 +33,14 @@ public class QuizPlatform extends Application {
        
         engine.setJavaScriptEnabled(true);
         
-        
-       
-        StackPane root = new StackPane(webView);
+        //StackPane root = new StackPane(webView);
         //root.getChildren().add(webView);
+        ProgressBar p2 = new ProgressBar();
+        p2.setProgress(0.25F);
+        BorderPane root = new BorderPane(webView, p2, null, null, null); 
+        root.setAlignment(p2,Pos.CENTER);
         Scene scene = new Scene(root,1000,800);
+        scene.getStylesheets().add("caspian.css");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
