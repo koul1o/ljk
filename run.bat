eafb27041d8af664e62156040d064ccf386d270d
koul1o <kyriakoskyriakopoulos@gmail.com>
2017-04-28 17:36:42 +0200

    -fixes in paths

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@
 
 @echo tTime=1 fTime=1 step=4
 @echo off
-java -jar "dist/QuizPlatform.jar" fullscreen=n tTime=1 fTime=1 root=math step=4
+java -jar "dist/QuizPlatform.jar" fullscreen=n tTime=1 fTime=1 step=4 root=psych
 pause
