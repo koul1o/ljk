bbf034c5ee074fb022cf4280034599af43c213af
koul1o <kyriakoskyriakopoulos@gmail.com>
2017-04-30 16:16:54 +0200

    -drop box added in info.html (for math and psych)

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@
 
 @echo tTime=1 fTime=1 step=4
 @echo off
-java -jar "dist/QuizPlatform.jar" fullscreen=n tTime=1 fTime=1 step=4 root=psych
+java -jar "dist/QuizPlatform.jar" fullscreen=y tTime=1 fTime=1 step=4 root=psych
 pause
